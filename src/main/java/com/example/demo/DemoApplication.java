651ed829f2f39c7152ece532878d11e1d7cf6d62
Shinnn2701 <nguyenmanhcz2701@gmail.com>
2025-07-26 10:52:05 +0700

    video 28

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,11 @@ import com.example.demo.student.Student;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	
 
+		ApplicationContext hoidanit = SpringApplication.run(DemoApplication.class, args);
+		for (String s : hoidanit.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
+	}
 
 }
